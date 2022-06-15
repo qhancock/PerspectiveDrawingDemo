61fbf423caf1e645de2a52224a5ca83a7931582e
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-06-14 22:49:07 -0400

    Added working buttons

--- scripts/js/Options.js
+++ scripts/js/Options.js
@@ -1,22 +1,57 @@
 const numPerspectives = 3;
-var selectedPerspective = 1;
-var perspectiveButtons = new HTMLButtonElement[numPerspectives];
+var perspectiveButtons;
+var guidesToggleButtons;
+var xrayToggleButtons;
+class OptionButtonSet {
+    selectedButton;
+    buttons;
+    constructor(buttons) {
+        this.buttons = buttons;
+        let thisSet = this;
+        for (let button of buttons) {
+            if (button != null) {
+                button.addEventListener("click", function () { thisSet.select(this); }, false);
+            }
+        }
+    }
+    select(selectedButton) {
+        if (this.selectedButton == selectedButton) {
+            return;
+        }
+        if (this.selectedButton != null) {
+            this.setDeselectedButtonAttributes(this.selectedButton);
+        }
+        this.selectedButton = selectedButton;
+        this.setSelectedButtonAttributes(this.selectedButton);
+    }
+    setSelectedButtonAttributes(button) {
+        button.style.border = "4px solid black";
+        button.style.fontWeight = "500";
+    }
+    setDeselectedButtonAttributes(button) {
+        button.style.border = "2px solid black";
+        button.style.fontWeight = "300";
+    }
+}
 initOptions();
 //initialize perspective button array
 function initOptions() {
-    for (let persp = 1; persp <= 3; persp++) {
-        perspectiveButtons[persp] = document.getElementById(persp + "PP");
-        perspectiveButtons[persp].addEventListener("click", function () { setPerspective(persp); });
+    //intialize perspective options
+    let perspectiveButtonList = new Array(4);
+    for (let persp = 1; persp <= numPerspectives; persp++) {
+        perspectiveButtonList[persp] = document.getElementById(persp + "PP");
+        // todo add event listener(s)
     }
-}
-function setPerspective(perspective) {
-    //guard clause exits if no change
-    if (perspective == selectedPerspective) {
-        return;
-    }
-    console.log(perspective);
-}
-function toggleGuides(on) {
-}
-function toggleXRay(on) {
+    perspectiveButtons = new OptionButtonSet(perspectiveButtonList);
+    perspectiveButtons.select(perspectiveButtons.buttons[1]);
+    //initialize guides options
+    let guidesOn = document.getElementById("guides_on");
+    let guidesOff = document.getElementById("guides_off");
+    guidesToggleButtons = new OptionButtonSet([guidesOn, guidesOff]);
+    guidesToggleButtons.select(guidesOn);
+    //initialize xray options
+    let xrayOn = document.getElementById("xray_on");
+    let xrayOff = document.getElementById("xray_off");
+    xrayToggleButtons = new OptionButtonSet([xrayOn, xrayOff]);
+    xrayToggleButtons.select(xrayOn);
 }
--- scripts/src/Options.ts
+++ scripts/src/Options.ts
@@ -1,30 +1,72 @@
 const numPerspectives = 3;
-var selectedPerspective = 1;
-var perspectiveButtons: HTMLButtonElement[] = new HTMLButtonElement[numPerspectives];
+var perspectiveButtons: OptionButtonSet;
+var guidesToggleButtons: OptionButtonSet;
+var xrayToggleButtons: OptionButtonSet;
+
+class OptionButtonSet {
+	selectedButton: HTMLButtonElement;
+	buttons: HTMLButtonElement[];
+
+	constructor(buttons: HTMLButtonElement[]) {
+		this.buttons = buttons;
+
+		let thisSet: OptionButtonSet = this;
+
+		for(let button of buttons) {
+			if(button!=null){
+				button.addEventListener("click", function(){thisSet.select(this)}, false);
+			}
+		}
+	}
+
+	public select(selectedButton: HTMLButtonElement): void {
+		if(this.selectedButton==selectedButton) {
+			return;
+		}
+		if(this.selectedButton!=null){
+			this.setDeselectedButtonAttributes(this.selectedButton);
+		}
+
+		this.selectedButton = selectedButton;
+
+		this.setSelectedButtonAttributes(this.selectedButton);
+	}
+
+	private setSelectedButtonAttributes(button: HTMLButtonElement): void {
+		button.style.border = "4px solid black";
+		button.style.fontWeight = "500";
+	}
+	private setDeselectedButtonAttributes(button: HTMLButtonElement): void {
+		button.style.border = "2px solid black";
+		button.style.fontWeight = "300";
+	}
+
+}
+
 initOptions();
 
 //initialize perspective button array
 function initOptions(): void {
 	
-	for (let persp = 1; persp <= 3; persp++) {
-		perspectiveButtons[persp] = document.getElementById(persp + "PP") as HTMLButtonElement;
-		perspectiveButtons[persp].addEventListener("click", function(){setPerspective(persp)});
+	//intialize perspective options
+	let perspectiveButtonList: HTMLButtonElement[] = new Array(4);
+	for(let persp = 1; persp<=numPerspectives; persp++) {
+		perspectiveButtonList[persp] = document.getElementById(persp + "PP") as HTMLButtonElement;
+		// todo add event listener(s)
 	}
+	perspectiveButtons = new OptionButtonSet(perspectiveButtonList);
+	perspectiveButtons.select(perspectiveButtons.buttons[1]);
+	
+	//initialize guides options
+	let guidesOn: HTMLButtonElement = document.getElementById("guides_on") as HTMLButtonElement;
+	let guidesOff: HTMLButtonElement = document.getElementById("guides_off") as HTMLButtonElement;
+	guidesToggleButtons = new OptionButtonSet([guidesOn, guidesOff]);
+	guidesToggleButtons.select(guidesOn);
 
-}
-
-function setPerspective(perspective: number): void {
-	//guard clause exits if no change
-	if (perspective == selectedPerspective) {
-		return;
-	}
-
-	console.log(perspective);
-
-}
-function toggleGuides(on: boolean): void {
-
-}
-function toggleXRay(on: boolean): void {
-
+	//initialize xray options
+	let xrayOn: HTMLButtonElement = document.getElementById("xray_on") as HTMLButtonElement;
+	let xrayOff: HTMLButtonElement = document.getElementById("xray_off") as HTMLButtonElement;
+	xrayToggleButtons = new OptionButtonSet([xrayOn, xrayOff]);
+	xrayToggleButtons.select(xrayOn);
+	
 }
