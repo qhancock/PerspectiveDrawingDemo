8ea0d858a6a36f75e21aa4cd6e441b6f496f5a2b
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-06-17 03:40:55 -0400

    fixed variable collisions via ordering

--- scripts/js/PerspectiveDrawingDemo.js
+++ scripts/js/PerspectiveDrawingDemo.js
@@ -1,7 +1,24 @@
 import { initOptions } from "./Options.js";
 import { Viewer } from "./Viewer.js";
+import { perspectiveButtons, guidesToggleButtons, xrayToggleButtons } from "./Options.js";
+import { reset as resetOnePointPerspective } from "./OnePointPerspective.js";
 
-initOptions();
 
 var canvasElement = document.getElementById("viewer");
 export var viewer = new Viewer(canvasElement);
+
+initOptions();
+resetOnePointPerspective();
+
+let initialPerspective = 1;
+perspectiveButtons.select(perspectiveButtons.buttons[initialPerspective]);
+viewer.requestNewViewerData(initialPerspective);
+viewer.setPerspective(initialPerspective);
+
+guidesToggleButtons.select(guidesToggleButtons.buttons[false?0:1]);
+viewer.toggleGuides(false);
+
+xrayToggleButtons.select(xrayToggleButtons.buttons[false?0:1]);
+viewer.toggleXray(false);
+
+viewer.drawData(viewer.activeViewerData);
