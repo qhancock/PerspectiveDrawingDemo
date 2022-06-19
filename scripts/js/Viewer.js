07a15b06d7b764ea0ec4529efe19c28e81ffaaad
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-06-19 01:56:29 -0400

    added mouse dragging
    pass-by-reference fixes

--- scripts/js/Viewer.js
+++ scripts/js/Viewer.js
@@ -1,5 +1,5 @@
 import { Point } from "./Geometry.js";
-import { getViewerData as onePointPerspectiveViewerData, reset } from "./OnePointPerspective.js";
+import { getViewerData as onePointPerspectiveViewerData, reset as onePointReset, face as onePointFace } from "./OnePointPerspective.js";
 
 export class Viewer {
 
@@ -8,8 +8,6 @@ export class Viewer {
 	xray;
 	canvasElement;
 	canvas;
-
-	activeViewerData;
 
 	onePointViewerData;
 	twoPointViewerData;
@@ -20,12 +18,25 @@ export class Viewer {
 	guideColor = 0x66cdaa;
 	xrayGuideColor = 0xabd1c4;
 
+	mouseDragging;
+
 	constructor(canvasElement) {
 		this.canvasElement = canvasElement;
 		this.canvas = canvasElement.getContext("2d");
 
 		this.canvasElement.width = canvasElement.clientWidth;
 		this.canvasElement.height = canvasElement.clientHeight;
+
+		let thisViewer = this;
+		this.canvasElement.addEventListener("mousedown", function() {thisViewer.mouseDragging = true});
+		this.canvasElement.addEventListener("mouseup", function() {thisViewer.mouseDragging = false})
+		this.canvasElement.addEventListener("mouseout", function() {thisViewer.mouseDragging = false});
+		this.canvasElement.addEventListener("mousemove", function() {
+			if(thisViewer.mouseDragging) {
+				let dragEvent = arguments[0];
+				thisViewer.moveActiveHandle(dragEvent.movementX, dragEvent.movementY);
+			}
+		});
 	}
 
 	clear() {
@@ -39,11 +50,11 @@ export class Viewer {
 	getHeight() {
 		return this.canvasElement.height;
 	}
-
+	
 	getArea() {
 		return (this.getHeight() * this.getWidth());
 	}
-
+	
 	getCenter() {
 		return new Point(this.getWidth() / 2, this.getHeight() / 2);
 	}
@@ -65,14 +76,25 @@ export class Viewer {
 			}
 		}
 	}
-
+	
+	getActiveViewerData() {
+		switch (this.activePerspective) {
+			case 1: {
+				return this.onePointViewerData;
+			}
+			case 2: {
+				return this.twoPointViewerData;
+			}
+			case 3: {
+				return this.threePointViewerData;
+			}
+		}
+	}
+	
 	drawData(viewerData) {
-
-		// console.log("Guides: " + this.guides + ", Xray: " + this.xray);
-		console.log(viewerData);
 		
 		this.clear();
-
+		
 		for (let edge of viewerData.edges) {
 			this.drawLine(edge, this.edgeColor);
 		}
@@ -110,21 +132,24 @@ export class Viewer {
 		}
 	}
 
-	setActiveViewerData(perspective) {
-		switch (perspective) {
+
+	moveActiveHandle(dx, dy) {
+		switch (this.activePerspective) {
 			case 1: {
-				this.activeViewerData = this.onePointViewerData;
+				onePointFace.translate(dx, dy);
 				break;
 			}
 			case 2: {
-				this.activeViewerData = this.twoPointViewerData;
+				// this.twoPointFaceHandle.translate(dx, dy);
 				break;
 			}
 			case 3: {
-				this.activeViewerData = this.threePointViewerData;
+				// this.threePointFaceHandle.translate(dx, dy);
 				break;
 			}
 		}
+		this.requestNewViewerData(this.activePerspective);
+		this.drawData(this.getActiveViewerData());
 	}
 
 	setPerspective(perspective) {
@@ -134,8 +159,7 @@ export class Viewer {
 			return;
 		}
 		this.activePerspective = perspective;
-		this.setActiveViewerData(this.activePerspective);
-		this.drawData(this.activeViewerData);
+		this.drawData(this.getActiveViewerData());
 
 	}
 
@@ -147,7 +171,7 @@ export class Viewer {
 		}
 		this.guides = on;
 
-		this.drawData(this.activeViewerData);
+		this.drawData(this.getActiveViewerData());
 	}
 
 	toggleXray(on) {
@@ -158,6 +182,6 @@ export class Viewer {
 		}
 		this.xray = on;
 
-		this.drawData(this.activeViewerData);
+		this.drawData(this.getActiveViewerData());
 	}
 }
