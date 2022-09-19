c8cfd16cd8d00fd869593ee073f5f62bb7663b1d
Quinn Hancock <90471837+qhancock@users.noreply.github.com>
2022-09-19 09:46:02 -0400

    uses common perspective geometry

--- scripts/js/OnePointPerspective.js
+++ scripts/js/OnePointPerspective.js
@@ -1,95 +1,61 @@
-import { LineSegment, Point, Square, Polygon } from "./Geometry.js";
-import { getViewer } from "./PerspectiveDrawingDemo.js";
+import * as Geometry from "./Geometry.js";
+import getComputedVertices, {FigureEdge, FigureFace} from "./PerspectiveGeometry.js";
 
-var vp;
+var VP;
 export var face;
 
 export function reset() {
-	vp = getViewer().getCenter().getRelativePoint(0, -getViewer().getHeight()/6);
-	let faceSide = Math.sqrt(getViewer().getArea())/10;
-	let faceCenter = getViewer().getCenter().getRelativePoint(100, getViewer().getHeight()/6);
-	face = new Square(faceCenter, faceSide);
+	VP = globalThis.viewer.getCenter().getRelativePoint(0, -globalThis.viewer.getHeight()/6);
+	let faceSide = Math.sqrt(globalThis.viewer.getArea())/10;
+	let faceCenter = globalThis.viewer.getCenter().getRelativePoint(100, globalThis.viewer.getHeight()/6);
+	face = new Geometry.Square(faceCenter, faceSide);
 }
 
 export function getViewerData() {
 
 	let nonFaceEdges = [];
 
-	for(let backEdge of getBackFace().getEdges()) {
+	let backFace = getBackFace();
+
+	for(let backEdge of backFace.getEdges()) {
 		nonFaceEdges.push(backEdge);
 	}
-	for(let depthEdge of getCubeDepthEdges()) {
+	for(let depthEdge of getCubeDepthEdges(backFace)) {
 		nonFaceEdges.push(depthEdge);
 	}
 
-	let edges = face.getEdges();
-	let xrayEdges = [];
+	let viewerData = [];
+
+	let faceEdges = face.getEdges();
+	for(let faceEdge of faceEdges) {
+		let figureEdge = new FigureEdge(faceEdge)
+		figureEdge.front = true;
+		viewerData.push(figureEdge);
+	}
 
 	for(let edge of nonFaceEdges) {
-
-		let xray = face.contains(edge.start)||face.contains(edge.end);
-
-		if(xray) {
-			xrayEdges.push(edge);
-		} else {
-			edges.push(edge);
-		}
-	}
-
-	let guides = [];
-	let xrayGuides = [];
-
-	for(let guide of getGuides()) {
-		if(face.contains(guide.start)||face.contains(guide.end)) {
-			xrayGuides.push(guide);
-		} else {
-			guides.push(guide);
-		}
-	}
-
-	let viewerData = {
-		"edges" : edges,
-		"xrayEdges" : xrayEdges,
-		"guides" : guides,
-		"xrayGuides" : xrayGuides 
+		edge.xray = face.contains(edge.start)||face.contains(edge.end);
+		viewerData.push(edge);
 	}
 
 	return viewerData;
 
 }
 
-function backVertexRayScalar(faceVertex) {
-	let sideFovRatio = 2/Math.PI * Math.atan(face.side/(Point.distance(face.center, vp)+50));
-	return 1 - sideFovRatio;
-}
-function backVertex(faceVertex) {
-	let scalar = backVertexRayScalar(face.center);
-	return vp.getRelativePoint(scalar*Point.dx(vp, faceVertex), scalar*Point.dy(vp, faceVertex));
-}
 function getBackFace() {
-	let backFacePoints = [];
-
-	for(let faceVertex of face.points) {
-		backFacePoints.push(backVertex(faceVertex));
-	}
-
-	return new Polygon(backFacePoints);
+	let backFacePoints = getComputedVertices(face.side, VP, face.points);
+	return new Geometry.Polygon(backFacePoints);
 }
-function getCubeDepthEdges() {
-	let cubeDepthSegments = [];
+
+function getCubeDepthEdges(backFace) {
+	let cubeDepthEdges = [];
 
 	for(let vIndex in face.points) {
-		cubeDepthSegments.push(new LineSegment(face.points[vIndex], getBackFace().points[vIndex]));
+		let edge = new Geometry.LineSegment(face.points[vIndex], backFace.points[vIndex]);
+		let figureEdge = new FigureEdge(edge, VP, 1);
+
+		cubeDepthEdges.push(figureEdge);
 	}
 
-	return cubeDepthSegments;
-}
-function getGuides() {
-	let guideSegments = [];
-
-	for(let vIndex in getBackFace().points) {
-		guideSegments.push(new LineSegment(getBackFace().points[vIndex], vp));
-	}
-
-	return guideSegments;
+	return cubeDepthEdges;
 }
